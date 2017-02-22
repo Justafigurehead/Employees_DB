3ba5330722814bf5145079b5b59cd99d6696a7d3
Justafigurehead <strachanmichaela@hotmail.com>
2017-02-22 16:33:08 +0000

    Added Save and All to Employee and Department

---
+++ Department.java
@@ -0,0 +1,45 @@
+import java.sql.ResultSet;
+
+import javax.xml.transform.Result;
+
+import db.SqlRunner;
+
+public class Department {
+    private int id;
+    private String title;
+
+    public Department(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void save() {
+        String sql = String.format("INSERT INTO departments (title) VALUES ('%s');", this.title);
+        this.id = SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public static void all(){
+        String sql = "SELECT * FROM departments;";
+        ResultSet rs = SqlRunner.executeQuery(sql);
+        try {
+            while (rs.next()) {
+                String title = rs.getString("title");
+                System.out.println(title);
+                System.out.println();
+            }
+        }catch (Exception e) {
+            System.err.println(e.getClass().getName() + " : " + e.getMessage());
+            System.exit(0);
+        } finally {
+            SqlRunner.closeConnection();
+        }
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,61 @@
+import java.sql.ResultSet;
+
+import db.SqlRunner;
+
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+    private Department department;
+
+    public Employee(String name, Department department, double salary) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void save() {
+        int department_id = department.getId();
+        String sql = String.format("INSERT INTO employees(name, salary, department_id) VALUES ('%s', %7.2f, %d)", this.name, this.salary, department_id);
+        this.id = SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public static void all() {
+        String sql = "SELECT * FROM employees;";
+        ResultSet rs = SqlRunner.executeQuery(sql);
+        try {
+            while (rs.next()) {
+                String name = rs.getString("name");
+                Double salary = rs.getDouble("salary");
+                int pay = salary.intValue();
+                System.out.println(name);
+                System.out.println(pay);
+                System.out.println();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ":" + e.getMessage());
+            System.exit(0);
+        } finally {
+            SqlRunner.closeConnection();
+        }
+
+    }
+}
+
